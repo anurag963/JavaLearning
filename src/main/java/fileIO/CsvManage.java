8d51ebddd7649c283d26a80bf75096eee3a33510
anurag <anurag105031@gmail.com>
2021-09-18 21:41:33 +0530

    Added text file reader

--- src/main/java/fileIO/CsvManage.java
+++ src/main/java/fileIO/CsvManage.java
@@ -1,16 +1,10 @@
-package FileIO;
+package fileIO;
 
-import com.opencsv.CSVReader;
 import com.opencsv.CSVWriter;
 import com.opencsv.exceptions.CsvValidationException;
 import org.testng.annotations.Test;
 
 import java.io.*;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.Arrays;
 
 public class CsvManage {
 
